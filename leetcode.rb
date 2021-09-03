f81f97877e1a1a972d753ff72ee3e1b9965b4056
DrinE Liu <drine@client.com.au>
2021-09-03 23:59:50 +0800

    common prefix

---
+++ leetcode.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+# https://leetcode.com/problems/longest-common-prefix/
+def longest_common_prefix(strs)
+  return "" if strs.length == 0
+  l = 0
+  min_str = strs.min_by(&:length)
+  r = min_str.length - 1
+  while  l <= r
+    # 二分
+    m = (l + r) / 2
+    if strs.all?{|str| str[m] == min_str[m]}
+      l = m+1
+    else
+      r = m-1
+    end
+  end
+  r >= 0 && strs.all?{|str| str[min_str[0..r]]} ? min_str[0..r] : ""
+end
+
